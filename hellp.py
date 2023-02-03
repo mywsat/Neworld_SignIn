9a0d23e3e7325bab1a9f996770cf6b07938c013f
Wsat <wsat@foxmail.com>
2023-02-03 15:04:47 +0800

    Update hellp.py

--- hellp.py
+++ hellp.py
@@ -14,7 +14,7 @@ chrome_options.add_argument('--window-size=1420,1080')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 # action  linux服务器驱动地址
-s = Service("/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver")
+s = Service('/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver')
 driver = webdriver.Chrome(service=s)    # Chrome浏览器  
 # driver = webdriver.Chrome(executable_path='/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver')    # Chrome浏览器  
 
